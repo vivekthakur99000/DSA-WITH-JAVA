3893eedfecd0f6266e5c62c3549e8c5484babf15
vivek thakur <vivekthakur99000@gmail.com>
2023-09-08 14:40:25 +0530

    primitives data types

--- src/Primitives.java
+++ src/Primitives.java
@@ -1,5 +1,10 @@
 public class Primitives {
     public static void main(String[] args) {
-
+        int rollNum = 64;
+        char letter  = 'v'; // single quotes is used to store character values
+        float marks = 98.67f; // used to store decimal values with f
+        double largeDecimalNo = 4575406.25;
+        long largeInt = 3535769673543215L; // used to store large integar  values with L
+        boolean check = true;
     }
 }
