a99058266a46c90ad4bee7f0e2f281343aa2ade2
vivek thakur <vivekthakur99000@gmail.com>
2023-09-08 14:36:21 +0530

    primitives data types

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 //        FIRST PROGRAM
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
 
+
+//        TO TAKE A INPUT FOR USER
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter your name ");
+        String name = sc.next();        //        it will only print only one line
+        String fullName = sc.nextLine(); // it will print the whole line
+        System.out.println("Hello " +name);
     }
 }
